abdaa68eb2089407bd7c873d89cc4028b030d883
valetzx <45054400+valetzx@users.noreply.github.com>
2022-02-17 19:43:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ cat <<-EOF > /root/cloudreve/conf.ini
 Mode = master
 Listen = :${PORT}
 [Redis]
-Server = ${REDIS_URL##*@}
-Password = ${REDIS_URL:9:65}
+Server = 127.0.0.1:6379
+Password = 
 DB = 0
 [Database]
 Type = mysql
-Host = ${JAWSDB_URL:42:57}
-Port = ${JAWSDB_URL:100:4}
-User = ${JAWSDB_URL:8:16}
-Password = ${JAWSDB_URL:25:16}
-Name = ${JAWSDB_URL##*/}
+Host = containers-us-west-28.railway.app
+Port = 7336
+User = root
+Password = ELWKs5fiISVGienNC2UR
+Name = railway
 Charset = utf8
 TablePrefix = cd
 EOF
